87732315be2ac3d02fccb14a993897b9d71d11c5
Nelvin Driz <nelvindriz@live.com>
2012-03-30 22:49:41 +0800

    Initial Commit
    Signed-off-by: Nelvin Driz <nelvindriz@live.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneBasic::Application.config.secret_token = '9e9bd83c9170bee370fcc14eaed8776d96fe4a8210df6d0c5e43520ef77b6de5815bcf995f41ba29f0137d6ca595ad864686ee852c2976404362ce676d5b421d'
